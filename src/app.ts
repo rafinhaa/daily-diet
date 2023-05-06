53080e8841086c2bf3d877e3d4e6c07dec96a0c4
Rafinhaa <rafinhaa@me.com>
2023-05-06 11:25:19 -0300

    feat: Variáveis de ambiente

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,23 @@
 import fastify from "fastify";
+import { env } from "./env";
 
-export const app = fastify({});
+const envToLogger = {
+  development: {
+    transport: {
+      target: "pino-pretty",
+      options: {
+        translateTime: "HH:MM:ss Z",
+        ignore: "pid,hostname",
+      },
+    },
+  },
+  production: true,
+  test: false,
+};
+
+export const app = fastify({
+  logger: envToLogger[env.NODE_ENV],
+});
 
 app.get("/", async (req, res) => {
   res.send("Hello World!");
